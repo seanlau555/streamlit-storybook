787fe3a0705373df957bf103c69f5c953f146aa4
Sean Lau <lthon0225@gmail.com>
2022-02-08 13:24:52 +0800

    use simple for external

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,25 @@
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
 
-const packageJson = require('./package.json');
 
 export default [
   {
     input: './src/index.ts',
     output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            sourcemap: true,
-            name: 'react-lib'
-        },
-        {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true
-        }
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.es.js',
+        format: 'es',
+        exports: 'named',
+      }
     ],
     plugins: [
       external({
